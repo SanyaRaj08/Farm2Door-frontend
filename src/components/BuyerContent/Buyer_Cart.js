b59fc9e9a2357e1d5cc83e1b1648772d96311061
SanyaRaj08 <rajsanya05@gmail.com>
2023-11-26 17:03:31 +0530

    latest commit

--- src/components/BuyerContent/Buyer_Cart.js
+++ src/components/BuyerContent/Buyer_Cart.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import "../../style/Buyer_Content.css";
+import { Link } from "react-router-dom";
 
 const Buyer_Cart = () => {
   return (
-    <div>
-      
-    </div>
+    <ChakraProvider>
+      <Box display="flex">
+        <Box
+          display="flex"
+          flexDirection="column"
+          color="#665039"
+          className="buyer_content"
+          paddingLeft="10"
+          paddingTop="20"
+        >
+          <Box className="buyer_content_options" padding="5" paddingTop="10" >
+            <Link to="/BuyerContent">
+              <i class="fa-solid fa-house"></i> Home
+            </Link>
+          </Box>
+          <Box className="buyer_content_options" padding="5" color="#e8c897">
+          <Link to="/BuyerCart">
+          <i class="fa-solid fa-cart-shopping"></i>  Cart
+          </Link>
+          
+          </Box>
+          <Box className="buyer_content_options" padding="5">
+          <Link to="/BuyerAccount">
+          <i class="fa-solid fa-user"></i>  Account
+          </Link>
+          
+          </Box>
+        </Box>
+        <Box></Box>
+      </Box>
+    </ChakraProvider>
   )
 }
 
--- src/components/BuyerContent/Buyer_Home.js
+++ src/components/BuyerContent/Buyer_Home.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import "../../style/Buyer_Content.css";
+import { Link } from "react-router-dom";
 
 const Buyer_Home = () => {
   return (
     <ChakraProvider>
       <Box display="flex">
-        <Box display="flex" flexDirection="column" bg="#e8c897" color="#665039" fontWeight="bold" fontSize="2xl">
-          <Box>
-            Home
+        <Box
+          display="flex"
+          flexDirection="column"
+          color="#665039"
+          className="buyer_content"
+          paddingLeft="10"
+          paddingTop="20"
+        >
+          <Box className="buyer_content_options" padding="5" paddingTop="10" color="#e8c897">
+            <Link to="/BuyerContent">
+              <i class="fa-solid fa-house"></i> Home
+            </Link>
           </Box>
-          <Box>
-            Cart
+          <Box className="buyer_content_options" padding="5">
+          <Link to="/BuyerCart">
+          <i class="fa-solid fa-cart-shopping"></i>  Cart
+          </Link>
+          
           </Box>
-          <Box>
-            Account
+          <Box className="buyer_content_options" padding="5">
+          <Link to="/BuyerAccount">
+          <i class="fa-solid fa-user"></i>  Account
+          </Link>
+          
           </Box>
         </Box>
         <Box></Box>
